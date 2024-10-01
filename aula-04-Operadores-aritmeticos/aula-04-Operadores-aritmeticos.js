4ba9df6904eb80ebaa7a271d636718cab901b094
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-01 11:28:53 -0300

    inicio da aula 04

---
+++ aula-04-Operadores-aritmeticos/aula-04-Operadores-aritmeticos.js
@@ -0,0 +1,31 @@
+"Use strict"
+
+/*
+Variáveis declaradas com var têm escopo global ou de função, enquanto variáveis declaradas com let têm escopo de bloco.
+
+variaveis declaradas com const não podem ser alteradas seus valores
+*/
+
+
+
+    let  nome = "Ancelmo";
+  
+    console.log(nome);
+
+    nome = "Ancelmo Viana";
+
+    console.log(nome);
+
+    const curso = " javascript";
+
+
+    console.log(curso);
+
+
+
+  
+
+
+
+
+
