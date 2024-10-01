4a7c7a4552f602d0d1ee39a3f775a853418505ab
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-01 12:05:37 -0300

    conclusao da aula 04

--- aula-04-Operadores-aritmeticos/aula-04-Operadores-aritmeticos.js
+++ aula-04-Operadores-aritmeticos/aula-04-Operadores-aritmeticos.js
@@ -1,30 +1,58 @@
-"Use strict"
-
 /*
-Variáveis declaradas com var têm escopo global ou de função, enquanto variáveis declaradas com let têm escopo de bloco.
++   soma
+-   subtração       
+/   divisão
+*   pontenciação
+%   resto da divisão
+++  incremento
+-- decremento
++= mais igual
+-=  menos igual 
 
-variaveis declaradas com const não podem ser alteradas seus valores
 */
 
+let num1 =0, num2=0, res=0;
+num1 = 10;
+num2 = 15;
+console.log("soma");
+
+res = num1 + num2;
+
+console.log(num1 + " + " + num2 + " é = "+ res);
+
+console.log("subtração")
+res = num2 - num1;
+
+console.log(num2 + " - " + num1 + " = " + res);
 
 
-    let  nome = "Ancelmo";
-  
-    console.log(nome);
+num1 = 2;
+num2 = 15;
+console.log("Resto da divisão")
+res = num2 % num1;
 
-    nome = "Ancelmo Viana";
+console.log("O resto da divisao de "+ num2 + " com "+ num1 +  " é = " + res);
 
-    console.log(nome);
+console.log("incremento");
 
-    const curso = " javascript";
+num1 = 10;
+num1++;
+
+console.log("o valor de num1 é " + (num1 - 1) +" se adicionarmos o operador ++ num++ seu valor será o valor de num1 + 1 neste caso = "+ num1 );
+
+num1 = 10;
+num1 --;
+
+console.log("Decremento");
+console.log("o valor de num1 é " + (num1 + 1) +" se adicionarmos o operador -- num-- seu valor será o valor de num1 - 1 neste caso = "+ num1 );
+
+num1 = 10;
+
+console.log("+=  fará o mesmo que o operador ++ porém neste caso estamos determinando o valor que será incrementado ex ");
+console.log("o valor de num1 é "+ num1 + " se colocarmos num1 += 5 por exemplo seu valor sera 10 + 5, ex num1+=5 " + (num1 += 5))
 
 
-    console.log(curso);
-
-
-
-  
-
+console.log("isso serve para + - / * % entre outros, qualquer operador que eu adicionar antes do igual sera a variavel + ou - ou * etc o valor adicionado depois ex, num1 = 10  num1*= 5 seu valor sera 10 multiplicado por 5 ou seja 50.")
 
 
 
