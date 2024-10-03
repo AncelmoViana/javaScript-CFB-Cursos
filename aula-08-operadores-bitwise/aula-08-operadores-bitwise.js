aea3145e48aa29f4acead2cf2d90b289bbdb2b4d
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-03 19:20:51 -0300

    conclusao aula 08

--- aula-08-operadores-bitwise/aula-08-operadores-bitwise.js
+++ aula-08-operadores-bitwise/aula-08-operadores-bitwise.js
@@ -1,34 +1,6 @@
-/*
+let n1=10;
+let n2 =11;
 
-Operadores lógicos
- && and e
- || or 0u
- ! mot não
-*/
-let n1, n2, n3, n4;
-n1 = 10;
-n2 = 5;
-n3 = 15;
-n4 = 2;
+let res = n1 & n2;
 
-console.log((n1 > n2) && (n1 > n3));
-
-console.log((n1 > n2) || (n1 > n3));
-
-console.log(!((n1 > n2) || (n1 > n3)));
-
-
-
-if(n1 > n2){
-    console.log(n1 + " é maior que " + n2)
-}
-else{
-    console.log(n1 + " è menor que " + n2);
-}
-
-if(n1>n2 && n3<n4){
-    console.log("Verdadeiro");
-}
-else{
-    console.log("Falso");
-}
+console.log(res);
