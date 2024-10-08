7a77d7015bde4e830f927f2521c76cb1552470d3
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-08 18:56:22 -0300

    conclusao da aula 11

--- aula-09-diferenca-entre-pre-incremento-e-pos-incremento/aula-09-diferenca-entre-pre-incremento-e-pos-incremento.js
+++ aula-09-diferenca-entre-pre-incremento-e-pos-incremento/aula-09-diferenca-entre-pre-incremento-e-pos-incremento.js
@@ -1,13 +1,9 @@
-let num = 10;
+let n = 10;
 
 
 
-let res = (num%2 ? "Impar"  : "Par")
-console.log(res);
+console.log(++n);
 
-let num2 =8;
-let num3 = 5;
+console.log(--n);
 
-let res2 = (num2 > num3 ? "verdade" : "Falso");
-console.log(res2);
 
--- aula-11-operador-typeof/aula-11-operador-typeof.js
+++ aula-11-operador-typeof/aula-11-operador-typeof.js
@@ -1,7 +1,14 @@
-let n = 10;
+let v1 = 10;
+let v2 = "10";
+let v3 = v1===v2;
+let v4 = {nome: "Ancelmo"};
 
 
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v1));
 
-console.log(++n);
+console.log("Valor: " + v2 + " - Tipo: " + typeof(v2));
 
-console.log(--n);
+console.log("Valor: " + v3 + " - Tipo: " + typeof(v3));
+
+console.log("Valor: " + v4 + " - Tipo: " + typeof(v4));
+
