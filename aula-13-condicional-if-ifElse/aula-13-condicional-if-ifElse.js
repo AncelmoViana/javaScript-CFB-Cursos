62fb9db9bdba80aaed64ae0a2dbab2a7a01220f8
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-08 20:12:38 -0300

    conclusão da aula 13

---
+++ aula-13-condicional-if-ifElse/aula-13-condicional-if-ifElse.js
@@ -0,0 +1,27 @@
+ let num = 45;
+
+ if(num > 10 ){
+    console.log("Número maior que 10");
+    if(num > 50){
+        console.log("Número maior que 50");
+    }
+ }
+ else if(num > 5){
+    console.log("Número está entre 6 e 10")
+
+ }
+ else{
+    console.log("Número menor ou igual a 5");
+ }
+ console.log("Fim de Programa");
+
+
+ let clima = "chovendo";
+ let energia = 100;
+
+ if(energia > 70 && clima == "sol"){
+    console.log("Eu vou a praia");
+ }
+ else{
+    console.log("Eu vou ao cinema");
+ }
