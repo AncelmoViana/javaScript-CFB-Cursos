34be179051053d03f5cd4221bf6da6b1e1468c2b
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-08 18:30:48 -0300

    fim da aula 10

--- aula-09-diferenca-entre-pre-incremento-e-pos-incremento/aula-09-diferenca-entre-pre-incremento-e-pos-incremento.js
+++ aula-09-diferenca-entre-pre-incremento-e-pos-incremento/aula-09-diferenca-entre-pre-incremento-e-pos-incremento.js
@@ -1,7 +1,13 @@
-let n = 10;
+let num = 10;
 
 
 
-console.log(++n);
+let res = (num%2 ? "Impar"  : "Par")
+console.log(res);
 
-console.log(--n);
+let num2 =8;
+let num3 = 5;
+
+let res2 = (num2 > num3 ? "verdade" : "Falso");
+console.log(res2);
+
