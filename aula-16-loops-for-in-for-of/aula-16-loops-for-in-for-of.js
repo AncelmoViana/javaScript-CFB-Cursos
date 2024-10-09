431c18eed03d9a170e9a567d9d352c8cfe8638a9
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-09 18:45:07 -0300

    conclusao da aula 16

--- aula-16-loops-for-in-for-of/aula-16-loops-for-in-for-of.js
+++ aula-16-loops-for-in-for-of/aula-16-loops-for-in-for-of.js
@@ -1,7 +1,7 @@
 let num =[10,20,30,40,50];
 
 for(let i=0; i<num.length; i++){
-   console.log(num[i]);
+   console.log("for tradicional " + num[i]);
 }
 
 for(n in num){
@@ -10,4 +10,14 @@ for(n in num){
 
 for(n of num){
    console.log("for of " + n);
+}
+
+const objs = document.getElementsByTagName("div");
+
+for(o of objs){
+   console.log(o.innerHTML = "cursos");
+}
+
+for(o in objs){
+   console.log(objs[0].innerHTML = "curso2 ")
 }
