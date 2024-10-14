1396cd1db04d35cda6235e0b51ecb0c85abd7852
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-14 10:24:59 -0300

    conclusao da aula 20

--- aula-20-FUNÇÕES-em-Javascript/aula-20-FUNÇÕES-em-Javascript.js
+++ aula-20-FUNÇÕES-em-Javascript/aula-20-FUNÇÕES-em-Javascript.js
@@ -1,38 +1,45 @@
-/*
-Break:
-quando a execuçao encontra um break no codigo ele para o loop encerrando-o e continua a execuçao do programa
-
-Continue:
-quando é executado dentro de um loop ele para a iteração e pula para a proxima iteração.
-*/
-
-let n = 0;
-let max = 1000;
-
-console.log("Exemplo de um break");
-
-let pares = 0;
-
-while(n<max){
-   console.log("CBF Cursos - " + n);
-   if(n > 10){
-      break;
-   }
-   n++;
+function nome(){
+   console.log("CFB Cursos");
 }
-console.log("Fim do Programa");
 
+nome();
 
-
-console.log("Exemplo de um Continue");
-for(let i=0; i<max; i++){
- 
-   if(i%2!=0){
-      continue;
-   }
-   pares++;
-
+for(let i=0; i<10; i++){
+   nome();
 }
-console.log("Quantidade de pares: " + pares)
-console.log("Fim do Programa")
 
+function soma(){
+   let n1 = 15;
+   let n2 = 10;
+   let soma = n1 + n2;
+   console.log(soma);
+}
+
+soma();
+
+cont = 0;
+let d1=document.getElementById("d1");
+let d2=document.getElementById("d2");
+let d3=document.getElementById("d3");
+let botao = document.getElementById("botao");
+
+function mudarTexto(){
+   if(cont == 0){
+      d1.innerHTML = "d1 alterado";
+      d2.innerHTML = "d2 alterado";
+      d3.innerHTML = "d3 alterado";
+      botao.innerHTML = "Voltar Texto"
+
+      cont = 1;
+
+   }
+   else{
+      d1.innerHTML = "Ancelmo";
+      d2.innerHTML = "CFB Cursos";
+      d3.innerHTML = "Curso de Javascript";
+       botao.innerHTML = "Mudar Texto"
+
+      cont = 0;
+   }
+   
+}
