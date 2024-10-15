8bcfa979fc292d481d23f31f35fda24a9f4309e5
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-15 10:12:21 -0300

    fim da aula 23

--- aula-23-Parametros-REST-em-funcoes/aula-23-Parametros-REST-em-funcoes.js
+++ aula-23-Parametros-REST-em-funcoes/aula-23-Parametros-REST-em-funcoes.js
@@ -1,5 +1,21 @@
-function soma(n1, n2){
-   return n1 + n2;
+function soma(...valores){
+   let tam = valores.length;
+   let res = 0;
+   for(let i=0; i<tam; i++){
+      res += valores[i];
+   }
+   return res
 }
 
-console.log((soma(10,5)))
+console.log((soma(2, 4, 6, 10)))
+
+
+function soma(...valores){
+   let res = 0;
+   for(let v of valores){
+      res += v;
+   }
+   return res
+}
+
+console.log((soma(2, 4, 6, 10, 8, 5 )))
