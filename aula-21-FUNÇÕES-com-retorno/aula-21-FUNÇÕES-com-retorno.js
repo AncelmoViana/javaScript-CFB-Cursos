a59451453ccd64b4882e97ab6e003a9b6449bb20
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-15 08:40:15 -0300

    conclusao da aula 21

--- aula-21-FUNÇÕES-com-retorno/aula-21-FUNÇÕES-com-retorno.js
+++ aula-21-FUNÇÕES-com-retorno/aula-21-FUNÇÕES-com-retorno.js
@@ -1,45 +1,14 @@
-function nome(){
-   console.log("CFB Cursos");
-}
+function canal(){
+   let n1 = 10;
+   let n2 = 3;
+   let res = n1 * n2;
 
-nome();
-
-for(let i=0; i<10; i++){
-   nome();
-}
-
-function soma(){
-   let n1 = 15;
-   let n2 = 10;
-   let soma = n1 + n2;
-   console.log(soma);
-}
-
-soma();
-
-cont = 0;
-let d1=document.getElementById("d1");
-let d2=document.getElementById("d2");
-let d3=document.getElementById("d3");
-let botao = document.getElementById("botao");
-
-function mudarTexto(){
-   if(cont == 0){
-      d1.innerHTML = "d1 alterado";
-      d2.innerHTML = "d2 alterado";
-      d3.innerHTML = "d3 alterado";
-      botao.innerHTML = "Voltar Texto"
-
-      cont = 1;
-
+   if(res%2==0){
+      return res + " é Par";
    }
    else{
-      d1.innerHTML = "Ancelmo";
-      d2.innerHTML = "CFB Cursos";
-      d3.innerHTML = "Curso de Javascript";
-       botao.innerHTML = "Mudar Texto"
-
-      cont = 0;
+      return res + " é Impar"
    }
-   
 }
+
+console.log(canal());
