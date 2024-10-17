63f2f05ae2f3f970c1d0109ab66568bc2fcc2a75
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-17 15:30:29 -0300

    criaçao da pasta da aula 26

---
+++ aula-26-Funções-dentro-de-funções/aula-26-Funções-dentro-de-funções.js
@@ -0,0 +1,11 @@
+
+console.log("função anônima tradicional");
+
+const soma = function(v1, v2){return v1 + v2}
+console.log(soma(5,8));
+
+console.log("Funçao anônima com Arrow function");
+
+const soma2 = (v2, v3) => {return v2 + v3};
+
+console.log(soma(5,10))
