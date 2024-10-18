92cd817652d91731ca033d76f1a52bfe9ea443f8
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-18 16:58:26 -0300

    conclusao da aula 28

--- aula-28-método-MAP/aula-28-método-MAP.js
+++ aula-28-método-MAP/aula-28-método-MAP.js
@@ -1,17 +1,39 @@
-function* cores(){
-    yield "Vermelho";
-    yield "Verde";
-    yield "Azul";
+// const cursos =["HTML", "CSS", "Javascript", "PHP", "React"];
+
+// cursos.map((el, i)=>{
+//     console.log("Curso: " + el + " - posiçao do curso: " + i)
+// } );
+
+
+// let el = document.getElementsByTagName("div");
+// el = [...el]
+
+// el.map((e, i) => {
+//     console.log(e.innerHtml);
+// })
+// console.log(el);
+
+// const el = document.getElementsByClassName("div");
+
+// const val = Array.prototype.map.call(el, ({innerHTML}) => innerHTML);
+
+// console.log(val)
+
+// const converterInt = (e) => parseInt(e);
+// let num = ['1', '2', '3' , '4', '5'].map(converterInt);
+
+// console.log(num);
+
+
+const pessoas = [
+    {id: 3, nome: 'Ancelmo'},
+    {id: 2, nome: 'Maria'},
+    {id: 1, nome: 'Helena'}
+];
+
+const novasPessoas = {};
+for(const {id, nome} of pessoas){
+    console.log(id, nome)
 }
-
-const itc = cores();
-console.log("Primeira chamada da função")
-console.log(itc.next().value);
-
-console.log("Segunda chamada da função")
-console.log(itc.next().value);
-
-console.log("Terceira chamada da função")
-console.log(itc.next().value);
 
 
