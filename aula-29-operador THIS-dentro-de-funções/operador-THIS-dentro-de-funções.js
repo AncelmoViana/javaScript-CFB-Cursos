a42e0f06a738bad80467f9410a6834df15f30c35
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-22 10:28:33 -0300

    conclusao da aula 29

--- aula-29-operador THIS-dentro-de-funções/operador-THIS-dentro-de-funções.js
+++ aula-29-operador THIS-dentro-de-funções/operador-THIS-dentro-de-funções.js
@@ -1,39 +1,15 @@
-// const cursos =["HTML", "CSS", "Javascript", "PHP", "React"];
+function aluno(nome, nota){
+    this.nome = nome;
+    this.nota = nota;
 
-// cursos.map((el, i)=>{
-//     console.log("Curso: " + el + " - posiçao do curso: " + i)
-// } );
-
-
-// let el = document.getElementsByTagName("div");
-// el = [...el]
-
-// el.map((e, i) => {
-//     console.log(e.innerHtml);
-// })
-// console.log(el);
-
-// const el = document.getElementsByClassName("div");
-
-// const val = Array.prototype.map.call(el, ({innerHTML}) => innerHTML);
-
-console.log(val)
-
-const converterInt = (e) => parseInt(e);
-let num = ['1', '2', '3' , '4', '5'].map(converterInt);
-
-console.log(num);
-
-
-const pessoas = [
-    {id: 3, nome: 'Ancelmo'},
-    {id: 2, nome: 'Maria'},
-    {id: 1, nome: 'Helena'}
-];
-
-const novasPessoas = {};
-for(const {id, nome} of pessoas){
-    console.log(id, nome)
+    this.dados_arrow = function(){
+        setTimeout(() => {
+         
+            console.log(this.nome);
+            console.log(this.nota);
+        }, 4000)
+    }
 }
-
+const al1 = new aluno("Ancelmo", 200);
+al1.dados_arrow();
 
