6844b6dc2c9fa88fafc67cdd0e1bda2f34a21bdb
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-23 10:14:07 -0300

    conclusao da aula 33

--- aula-33-querySelector-e-querySelectorAll/aula-33-querySelector-e-querySelectorAll.js
+++ aula-33-querySelector-e-querySelectorAll/aula-33-querySelector-e-querySelectorAll.js
@@ -1,15 +1,25 @@
-const cursosTodos = [...document.getElementsByClassName("curso")];
+const divTodos = [...document.getElementsByTagName("div")];
 
 const cursosc1 = [...document.getElementsByClassName("c1")];
 
 const cursosc2 = [...document.getElementsByClassName("c2")];
 
-const cursoEspecial = document.getElementsByClassName("curso")[1];
+const cursoEspecial = document.querySelector("#c1");
+cursoEspecial.innerHTML = "teste"
 
-console.log(cursosTodos);
-console.log(cursosc1);
-console.log(cursosc2);
-console.log(cursoEspecial);
+const query_divtodas = [...document.querySelectorAll("div,p")];
+const query_cursosTodos = [...document.querySelectorAll(".curso")];
+
+console.log(query_divtodas)
+console.log(query_cursosTodos);
+
+
+
+
+// console.log(divTodos);
+// console.log(cursosc1);
+// console.log(cursosc2);
+// console.log(cursoEspecial);
 
 cursosc2.map((el)=>{
     el.classList.add("destaque")
