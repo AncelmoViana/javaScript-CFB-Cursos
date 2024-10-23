af0d15b80372495e8b08f1666b451c1286b033aa
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-23 09:14:32 -0300

    conclusao da aula 31

--- aula-31-Método-getElementsByTagName/aula-31-Método-getElementsByTagName.js
+++ aula-31-Método-getElementsByTagName/aula-31-Método-getElementsByTagName.js
@@ -4,28 +4,13 @@ let dc3 = document.getElementById("c3");
 let dc4 = document.getElementById("c4");
 let dc5 = document.getElementById("c5");
 let dc6 = document.getElementById("c6");
+const arrayElementos = [dc1, dc2, dc3, dc4, dc5, dc6]
 
-
-
-
-console.log(dc1)
-console.log(dc1.id)
-console.log(dc1.innerHTML)
-
-dc1.innerHTML = "Testando alteraçao"
-
-const arrayElementos = [dc1, dc2, dc3, dc4, dc5, dc6];
-
+let colecaoHTML=[...document.getElementsByTagName("div")];
+console.log(colecaoHTML);
 console.log(arrayElementos);
 
-console.log("\n\nfor do array\n\n")
-
-for(d of arrayElementos){
-    console.log(d.innerHTML);
-}
-
-console.log("\n\nUsando o metodo map\n\n");
-
-arrayElementos.map((e) => {
-    console.log(e.innerHTML)
+console.log("\n\nUsando o metodo map\n\n")
+colecaoHTML.map((e)=>{
+    console.log(e.innerHTML);
 })
