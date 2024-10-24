415a7a109c63043476f84687493071790be95fcd
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-24 10:32:58 -0300

    inicio e pausa da aula 35

--- aula-35-praticando-addEventListener/aula-35-praticando-addEventListener.js
+++ aula-35-praticando-addEventListener/aula-35-praticando-addEventListener.js
@@ -1,18 +1,13 @@
-// const c1 = document.querySelector("#c1")
 const cursos = [...document.querySelectorAll(".curso")];
-
-// const msg =()=>{
-//     alert("Voce clicou")
-// }
-
-// c1.addEventListener("click", (evt)=>{alert('Você Clicou')
-//     const el = evt.target;
-//     el.classList.add("destaque")
-// })
+const botao = document.getElementById('botao');
 
 cursos.map((e)=>{
     e.addEventListener("click", (evt)=>{
     e.classList.add("destaque");
     console.log(e.innerHTML + " Foi Clicado ");
     })
+})
+
+botao.addEventListener("click", ()=>{
+    console.log(botao.parentElement )
 })
