ad9ee7b86458e3e15ae4b8bdec3a106675cb4c73
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-24 08:22:42 -0300

    criaçao da pasta treinamentos 24-10

---
+++ treinamentos/treinamentos-24-10-2024-metodo-map/treinamentos-24-10-2024-metodo-map.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4];
+
+const twice = numbers.map((number) => number * 2);
+
+console.log(twice);
+
+console.log("\n \n \n \nOutro Exemplo\n");
+
+const users = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Ancelmo'},
+    {id: 3, name: 'Pedro'}
+];
+
+console.log(users.map((user) =>user.name))
+
+
