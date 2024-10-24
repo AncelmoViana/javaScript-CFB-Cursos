0c86caeb173e48b3b67459af80e97ace3ac31ea7
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-24 09:35:58 -0300

    conclusao da aula 34

--- aula-34-addEventListener/aula-34-addEventListener.js
+++ aula-34-addEventListener/aula-34-addEventListener.js
@@ -1,26 +1,18 @@
-const divTodos = [...document.getElementsByTagName("div")];
+// const c1 = document.querySelector("#c1")
+const cursos = [...document.querySelectorAll(".curso")];
 
-const cursosc1 = [...document.getElementsByClassName("c1")];
+// const msg =()=>{
+//     alert("Voce clicou")
+// }
 
-const cursosc2 = [...document.getElementsByClassName("c2")];
+// c1.addEventListener("click", (evt)=>{alert('Você Clicou')
+//     const el = evt.target;
+//     el.classList.add("destaque")
+// })
 
-const cursoEspecial = document.querySelector("#c1");
-cursoEspecial.innerHTML = "teste"
-
-const query_divtodas = [...document.querySelectorAll("div,p")];
-const query_cursosTodos = [...document.querySelectorAll(".curso")];
-
-console.log(query_divtodas)
-console.log(query_cursosTodos);
-
-
-
-
-// console.log(divTodos);
-// console.log(cursosc1);
-// console.log(cursosc2);
-// console.log(cursoEspecial);
-
-cursosc2.map((el)=>{
-    el.classList.add("destaque")
+cursos.map((e)=>{
+    e.addEventListener("click", (evt)=>{
+    e.classList.add("destaque");
+    console.log(e.innerHTML + " Foi Clicado ");
+    })
 })
