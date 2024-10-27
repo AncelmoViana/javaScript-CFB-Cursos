f8835f7aa0af8613fa7e8a977afec9e834c4cdc5
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-27 17:48:01 -0300

    conclusao da aula 38

--- aula-38-entendo-relacao-dos-elementos-no-DOM/aula-38-entendo-relacao-dos-elementos-no-DOM.js
+++ aula-38-entendo-relacao-dos-elementos-no-DOM/aula-38-entendo-relacao-dos-elementos-no-DOM.js
@@ -1,16 +1,7 @@
 const caixa1 = document.querySelector("#caixa1");
-const btn_c1 = document.querySelector("#c1");
-
 const cursos = [...document.querySelectorAll(".curso")];
 
+console.log(caixa1.firstElementChild)
+console.log(caixa1.parentElement)
 
- cursos.map((e)=>{
-    e.addEventListener("click", (evento)=>{
-        evento.stopPropagation();
-        
-    })
- })
- caixa1.addEventListener("click", (evento)=>{
-    console.log("clicou")
-})
 
