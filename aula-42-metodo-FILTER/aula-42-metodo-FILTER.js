d23bacd8b5043277c2955d81b419220b00aa712e
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-10-31 19:56:33 -0300

    conclusao da aula 42

--- aula-42-metodo-FILTER/aula-42-metodo-FILTER.js
+++ aula-42-metodo-FILTER/aula-42-metodo-FILTER.js
@@ -1,29 +1,21 @@
- const caixa1 = document.querySelector("#caixa1");
- const btn_c = document.querySelectorAll(".cursos");
- const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"];
- 
- 
 
- cursos.map((el, chave) =>{
-    
-    console.log(chave)
-    const novoElemento = document.createElement("div");
+const words = ['spray', 'exuberante', 'destruição', 'present', 'elite' ];
 
-    caixa1.appendChild(novoElemento);
-    novoElemento.innerHTML = el;
+const result = words.filter((word) => word.length > 6);
 
-    novoElemento.setAttribute("id", "c"+chave);
-    novoElemento.setAttribute("class", "curso c1 ");
-   const btn_lixeira = document.createElement("img")
-   btn_lixeira.setAttribute("src", "./iconeLixeira.png")
-   btn_lixeira.setAttribute("class", "btn_lixeira")
-   novoElemento.appendChild(btn_lixeira)
-    btn_lixeira.addEventListener("click", (evt)=>{
-     
-      caixa1.removeChild(evt.target.parentNode)
-    })
+console.log(result);
 
 
-   
-   
- })
+const idades = [10, 15, 17, 20, 30, 40, 50, 60, 8];
+
+const maior = idades.filter((val)=>{
+  if(val>18){
+    return val;
+  }
+})
+const menor = idades.filter((val)=>val<18)
+
+console.log("\n\nArray completo "+idades)
+console.log("\n\nNúmeros maiores que 18 "+maior);
+console.log("\n\nNumeros menores que 18 "+menor)
+
