8656ff1c005d8c9c040476d52a7fba508d437c3a
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-11-10 16:56:45 -0300

    updated

--- treinamentos/treinamentos6-09-11-2024-metodo-filter/metodo-filter.js
+++ treinamentos/treinamentos6-09-11-2024-metodo-filter/metodo-filter.js
@@ -9,5 +9,63 @@ cursos.map((el, chave)=>{
     novoElemento.setAttribute("class", "cursos")
     novoElemento.setAttribute("id", "c"+chave)
 
+
+    const radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("id", "radio")
+    radio.setAttribute("name", "radio")
     
+    novoElemento.appendChild(radio);
+  
+
+    
+})
+
+const radioSelecionado=()=>{
+    const todosCursos =[...document.querySelectorAll("input[type=radio]")]
+      
+    let radio = todosCursos.filter((el)=>{
+        return el.checked;
+    })
+    return  radio[0];
+    
+    
+
+}
+
+
+
+const cursoSelecionado = document.getElementById("cursoSelecionado");
+
+cursoSelecionado.addEventListener("click", (evt)=>{
+    const rs = radioSelecionado();
+    if(rs!=undefined){
+        alert(rs.parentNode.firstChild.textContent)
+
+    }
+    else{
+        alert("Selecione um Curso!")
+    }
+
+    
+
+
+   
+
+     
+})
+
+const apagarCurso = document.getElementById("apagarCurso");
+
+apagarCurso.addEventListener("click", (evt)=>{
+    let rs = radioSelecionado();
+   
+    if(rs!=undefined){
+        rs = rs.parentNode
+        rs.remove();
+    }
+    else{
+        alert("Selecione um Curso")
+    }
+
 })
