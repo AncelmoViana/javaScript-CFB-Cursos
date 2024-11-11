443b52ed8704651a6131d5988dad80868a7a6d6e
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-11-11 17:22:57 -0300

    updated

--- treinamentos/treinamentos6-09-11-2024-metodo-filter/metodo-filter.js
+++ treinamentos/treinamentos6-09-11-2024-metodo-filter/metodo-filter.js
@@ -1,21 +1,30 @@
 const caixaElementos = document.getElementById("caixaElementos");
 const cursos = ["HTML", "CSS", "Javascript", "python", "MySQL"];
 
-cursos.map((el, chave)=>{
-  
-    const novoElemento = document.createElement("div");
-    novoElemento.innerHTML = el;
-    caixaElementos.appendChild(novoElemento)
-    novoElemento.setAttribute("class", "cursos")
-    novoElemento.setAttribute("id", "c"+chave)
-
+ let indice = 0;
+ const criarCurso = (curso)=>{
+    const novoCurso = document.createElement("div");
+    novoCurso.innerHTML = curso;
+    novoCurso.setAttribute("id", "c"+indice);
+    novoCurso.setAttribute("class", "cursos");
+   
 
     const radio = document.createElement("input");
     radio.setAttribute("type", "radio");
-    radio.setAttribute("id", "radio")
+    radio.setAttribute("class", "radio");
     radio.setAttribute("name", "radio")
-    
-    novoElemento.appendChild(radio);
+    novoCurso.appendChild(radio);
+
+    return novoCurso;
+ }
+
+
+cursos.map((el)=>{
+
+    const novoElemento = criarCurso(el);
+
+    caixaElementos.appendChild(novoElemento);
+   
   
 
     
@@ -69,3 +78,75 @@ apagarCurso.addEventListener("click", (evt)=>{
     }
 
 })
+
+
+const adicionarAntes = document.getElementById("adicionarAntes");
+const adicionarDepois = document.getElementById("adicionarDepois");
+let nomeCurso = document.getElementById("nomeCurso");
+
+adicionarAntes.addEventListener("click",(evt)=>{
+    const rs = radioSelecionado();
+
+    if(rs!=undefined){
+        if(nomeCurso.value != ""){
+            const cursoSelecionado = rs.parentNode;
+            const addcurso = criarCurso(nomeCurso.value);
+        
+            caixaElementos.insertBefore(addcurso, cursoSelecionado)
+    
+        }
+        else{
+            alert("Adicionar nome do Curso")
+        }
+       
+    }
+    else{
+        alert("Selecione um Curso")
+    }
+
+
+
+    
+   
+})
+
+
+adicionarDepois.addEventListener("click",()=>{
+    const rs = radioSelecionado();
+
+
+    if(rs!=undefined){
+        if(nomeCurso.value!=""){
+            const cursoSelecionado = rs.parentNode.nextSibling;
+            const addcurso = criarCurso(nomeCurso.value);
+            caixaElementos.insertBefore(addcurso, cursoSelecionado)
+
+        }
+        else{
+            alert("Adicionar nome do Curso")
+        }
+       
+    }
+    else{
+        alert("Selecione um Curso")
+    }
+
+    
+})
+
+const desmarcar = document.getElementById("desmarcar");
+
+desmarcar.addEventListener("click",()=>{
+    const rs=radioSelecionado();
+   
+    if(rs!=undefined){
+        rs.checked = false;
+    }
+
+})
+
+
+
+
+
+
