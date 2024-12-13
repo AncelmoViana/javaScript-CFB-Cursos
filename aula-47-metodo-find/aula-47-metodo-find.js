5711477557f11793895971928d12205664f543fa
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-12-13 16:24:11 -0300

    conclusao da aula 48

--- aula-47-metodo-find/aula-47-metodo-find.js
+++ aula-47-metodo-find/aula-47-metodo-find.js
@@ -1,8 +1,6 @@
  const p_array = document.querySelector("#array");
 
- const txt_pesquisar = document.querySelector("#txt_pesquisar");
-
- const btnPesquisar = document.querySelector("#btnPesquisar");
+ const btnVerificar = document.querySelector("#Verificar");
 
  const resultado = document.querySelector("#resultado");
 
--- aula-48-metodo-every/aula-48-metodo-every.js
+++ aula-48-metodo-every/aula-48-metodo-every.js
@@ -1,26 +1,25 @@
  const p_array = document.querySelector("#array");
 
- const txt_pesquisar = document.querySelector("#txt_pesquisar");
 
- const btnPesquisar = document.querySelector("#btnPesquisar");
+ const btnVerificar = document.querySelector("#btnVerificar");
 
- const resultado = document.querySelector("#resultado");
+ const resultado = document.getElementById("resultado")
 
- const elementos_array = [10,5,8,2,9,15,20,35,68,95,255];
+ const elementos_array = [40,5,8,2,9,15,20,35];
 
  p_array.innerHTML = "["+elementos_array+"]";
 
- btnPesquisar.addEventListener("click",(evt)=>{
-    resultado.innerHTML="Valor não encontrado"
-  const ret = elementos_array.find((e,i)=>{
+btnVerificar.addEventListener("click",(evt)=>{
   
-    if(txt_pesquisar.value==e){
-      resultado.innerHTML = "Valor encontrado "+e+" na posição "+i;
+  const ret=elementos_array.every((e,i)=>{
+    if(e<18){
+      resultado.innerHTML = "Array não conforme na posição "+i;
+      resultado.style.color = "#8B0000"
     }
-   
-    
-   
+    return e>=18
   })
-  console.log(ret);
-
- })
+  if(ret){
+    resultado.innerHTML = "Conforme!";
+    resultado.style.color = "#006400"
+  }
+})
