1ee92d81d2ba2de6d709a4fb293acc855263aae7
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-12-19 20:32:54 -0300

    conclusao da aula 53

--- aula-53-Populando-ARRAYS/aula-53-Populando-ARRAYS.js
+++ aula-53-Populando-ARRAYS/aula-53-Populando-ARRAYS.js
@@ -1,30 +1,48 @@
-let visor = document.getElementById("visor");
+let valor1 = document.getElementById("valor1").value;
+let valor2 = document.getElementById("valor2").value;
+
+let resultado = document.getElementById("resultado");
+
+let somar = document.getElementById("+");
+let multiplicar = document.getElementById("x");
 let diminuir = document.getElementById("-");
 let dividir = document.getElementById("/");
 
-let valores=[10,5];
+
+
+let valores=[valor1,valor2];
 const op=[
-    (val)=>{
-        let res=0;
-        for(v of val){
-            res+=v
-        }
-        return res;
+    ()=>{
+        const val=[document.getElementById("valor1").value,document.getElementById("valor2").value]
+        resultado.value=Number(val[0])+Number(val[1])
     },
-    (val)=>{
-        let res=1;
-        for(v of val){
-            res*=v
-        }
-        return res;
+    ()=>{
+        const val=[document.getElementById("valor1").value,document.getElementById("valor2").value]
+        resultado.value=Number(val[0])-Number(val[1])
     },
-    (val)=>{
-
-        for(v of val){
-            console.log(v)
-        }
-
+    ()=>{
+        const val=[document.getElementById("valor1").value,document.getElementById("valor2").value]
+        resultado.value=Number(val[0])*Number(val[1])
+    },
+    ()=>{
+        const val=[document.getElementById("valor1").value,document.getElementById("valor2").value]
+        resultado.value=Number(val[0])/Number(val[1])
     }
+   
+   
+   
 ];
 
 console.log(op[0](valores))
+somar.addEventListener("click",()=>{
+    console.log(op[0](valores))
+})
+diminuir.addEventListener("click",()=>{
+    console.log(op[1](valores))
+})
+multiplicar.addEventListener("click",()=>{
+    console.log(op[2](valores))
+})
+dividir.addEventListener("click",()=>{
+    console.log(op[3](valores))
+})
