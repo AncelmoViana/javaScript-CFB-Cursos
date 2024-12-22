0b1c87db6990e05f443aec88853b0c88cdaae95b
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-12-22 20:54:08 -0300

    updated

---
+++ treinamentos/treinamentos8-20-12-2024-Calculadora/treinamentos-20-12-2024-Calculadora.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+
+function teclas_calculadora(input){
+    display.value += input;
+}
+function apagarDisplay(){
+    display.value = "";
+
+}
+function calcular(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Erro";
+    }
+   
+
+}
