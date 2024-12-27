b853fe97a1cbfe959ef1ae26e05823156d256fb4
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-12-27 10:22:06 -0300

    updated

---
+++ treinamentos/treinamentos9-26-12-2024-Calculadora-parte-2/treinamentos9-26-12-2024-Calculadora-parte-2.js
@@ -0,0 +1,20 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function updateResult(originClear = false){
+    result.innerHTML = oringinClear ? 0 : currentNumber.replace(".",",");
+}
+
+buttons.forEach((button) =>{
+    button.addEventListener("click", ()=>{
+        const buttonText = button.innerText;
+        if (/^[0-9]+$/.test(buttonText)){
+            addDigit(buttonText);
+        }
+    })
+})
