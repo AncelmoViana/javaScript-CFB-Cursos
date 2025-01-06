1c1c7477f528858fa111fe2d4d0b76443c480cee
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2025-01-06 16:39:53 -0300

    updated

--- treinamentos/treinamentos13-06-01-2025-Objetos-em-js/treinamentos13-06-01-2025-Objetos-em-js.js
+++ treinamentos/treinamentos13-06-01-2025-Objetos-em-js/treinamentos13-06-01-2025-Objetos-em-js.js
@@ -1,12 +1,21 @@
-const display = document.getElementById("display");
-const botoes = [...document.querySelectorAll(".buttons")];
+const arr = new Array(1,2,3);
+const arr2 = new Array(4,5,6);
 
-botoes.forEach((evt)=>{
-    const btn = evt.firstChild.textContent;
-    
-    evt.addEventListener("click",()=>{
-        display.value += btn;
-    })
-})
+const data = new Date();
 
+class cadeira{
+    constructor(_cor){
+        this.cor = _cor;
+        this.data = new Date();
+    }
 
+    build(){
+        console.log(`Uma cadeira ${this.cor} foi construida às ${this.data.toLocaleString()}`)
+    }
+}
+
+const cadeira1 = new cadeira("azul")
+const cadeira2 = new cadeira("vermelha")
+
+cadeira1.build()
+cadeira2.build()
